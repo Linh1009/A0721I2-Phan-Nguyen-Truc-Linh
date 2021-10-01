c920abde58abea6b3b7623e045d0305a4cf66612
Linh10099 <linhdhpd02123@fpt.edu.vn>
2021-10-01 19:42:04 +0700

    Introduction to java

---
+++ Module 2/introduction_to_java/bai_tap/UngDungDocSoThanhChu.java
@@ -0,0 +1,12 @@
+package introduction_to_java.bai_tap;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class UngDungDocSoThanhChu {
+    public static void main(String[] args) {
+        System.out.println("Write number");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+    }
+
+}
