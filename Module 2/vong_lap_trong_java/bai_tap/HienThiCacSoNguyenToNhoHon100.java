18a78d6e4a5b5ac7d59c0559e8d5ee587ea92054
Linh10099 <linhdhpd02123@fpt.edu.vn>
2021-10-06 19:29:04 +0700

    Vòng lặp trong java

---
+++ Module 2/vong_lap_trong_java/bai_tap/HienThiCacSoNguyenToNhoHon100.java
@@ -0,0 +1,30 @@
+package vong_lap_trong_java.bai_tap;
+import java.util.Scanner;
+public class HienThiCacSoNguyenToNhoHon100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập n = ");
+        int n = scanner.nextInt();
+        System.out.printf("Tất cả các số nguyên tố nhỏ hơn %d là: \n", n);
+        for(int i = 0; i< n;i++){
+            if(isPrimeNumber(i)){
+                System.out.print(", " + i);
+            }
+
+        }
+
+    }
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
