c6563e1af9deb8862b45f79f3eb964be858540bc
Linh10099 <linhdhpd02123@fpt.edu.vn>
2021-10-19 16:36:43 +0700

    Ke Thua

---
+++ Module 2/ke_thua/bai_tap/lop_circle_va_lop_cylinder/TestCylinder.java
@@ -0,0 +1,9 @@
+package ke_thua.bai_tap.lop_circle_va_lop_cylinder;
+
+public class TestCylinder {
+    public static void main(String[] args) {
+        Circle cylinder = new Cylinder(2.4, "red", 10);
+        System.out.println("Area: " + cylinder.getArea());
+        System.out.println("Volume: " + ((Cylinder) cylinder).getVolumer());
+    }
+}
