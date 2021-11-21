3527abaf9d4571ce9f63c09b5da286a49b7cf72a
Linh10099 <linhdhpd02123@fpt.edu.vn>
2021-11-21 12:05:55 +0700

    DSA: Stack, Queue

---
+++ Module 2/dse_stack_queue/thuc_hanh/trien_khai_stack_su_dung_lop_linked_list_trong_thu_vien_java_util/MyGenericStack.java
@@ -0,0 +1,33 @@
+package dse_stack_queue.thuc_hanh.trien_khai_stack_su_dung_lop_linked_list_trong_thu_vien_java_util;
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList();
+    }
+
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+}
